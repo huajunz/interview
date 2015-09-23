3f771271355851f04af4c2e1261038ff6833e195
huajunz <huajunz@163.com>
2015-09-23 15:27:24 +0800

    add 0-1 package dp algorithm

---
+++ packageDP.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int v[200][200];//?i????????j???????????
+
+int max(int a,int b)
+{
+   if(a>=b)
+       return a;
+   else return b;
+}
+
+int KnapSack(int n,int w[],int p[],int x[],int M)
+{
+    int i,j;
+    for(i=0;i<=n;i++)	v[i][0]=0;
+    for(j=0;j<=M;j++)	v[0][j]=0;
+
+    for(i=0;i<=n-1;i++)
+	{
+		for(j=0;j<=M;j++)
+		{
+			if(j<w[i])  //?i????? ?????? ,???
+				v[i][j]=v[i-1][j];
+			else //??i?????????
+				v[i][j]=max(v[i-1][j],v[i-1][j-w[i]]+p[i]);
+		}
+	}
+
+    j=M;
+    for(i=n-1;i>=0;i--)
+    {
+        if(v[i][j]>v[i-1][j]) //???i???????????
+        {
+			x[i]=1;
+			j=j-w[i];
+        }
+		else x[i]=0;
+    }
+  
+    for(i=0;i<n;i++)
+        printf("%d ",x[i]);
+    printf("\n");
+	return v[n-1][M];     
+}
+
+void main()
+{
+    int s;//???????
+    int w[15];//?????
+    int p[15];//?????
+    int x[15];//???????
+    int n,i;
+    int M;//??????
+
+    scanf("%d %d",&M, &n);
+
+    for(i=0;i<n;i++)
+        scanf("%d",&w[i]);
+
+    for(i=0;i<n;i++)
+        scanf("%d",&p[i]);
+
+    s=KnapSack(n,w,p,x,M);
+
+    printf("%d\n",s); 
+}
+
+/* input
+
+5 4
+2 1 3 2
+3 2 4 2
+
+*/
+
+
+/* output
+
+0 1 0 1
+7
+
+*/
+
