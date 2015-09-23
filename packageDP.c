1e930bd316dbd288ee14f34da1ebc31b2418e834
huajunz <huajunz@163.com>
2015-09-23 15:35:57 +0800

    modified tab, and show chinese character correctly.

--- packageDP.c
+++ packageDP.c
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-int v[200][200];//?i????????j???????????
+int v[200][200];//前i个物品装入容量为j的背包中获得的最大价值
 
 int max(int a,int b)
 {
@@ -14,45 +14,45 @@ int max(int a,int b)
 int KnapSack(int n,int w[],int p[],int x[],int M)
 {
     int i,j;
-    for(i=0;i<=n;i++)	v[i][0]=0;
-    for(j=0;j<=M;j++)	v[0][j]=0;
+    for(i=0;i<=n;i++)   v[i][0]=0;
+    for(j=0;j<=M;j++)   v[0][j]=0;
 
     for(i=0;i<=n-1;i++)
-	{
-		for(j=0;j<=M;j++)
-		{
-			if(j<w[i])  //?i????? ?????? ,???
-				v[i][j]=v[i-1][j];
-			else //??i?????????
-				v[i][j]=max(v[i-1][j],v[i-1][j-w[i]]+p[i]);
-		}
-	}
+    {
+        for(j=0;j<=M;j++)
+        {
+            if(j<w[i])  //第i件物品大于 背包所余容量 ,则不选
+                v[i][j]=v[i-1][j];
+            else //若第i件物品能够放入背包
+                v[i][j]=max(v[i-1][j],v[i-1][j-w[i]]+p[i]);
+        }
+    }
 
     j=M;
     for(i=n-1;i>=0;i--)
     {
-        if(v[i][j]>v[i-1][j]) //???i???????????
+        if(v[i][j]>v[i-1][j]) //放入第i件物品使得总价值增加了
         {
-			x[i]=1;
-			j=j-w[i];
+            x[i]=1;
+            j=j-w[i];
         }
-		else x[i]=0;
+        else x[i]=0;
     }
   
     for(i=0;i<n;i++)
         printf("%d ",x[i]);
     printf("\n");
-	return v[n-1][M];     
+    return v[n-1][M];     
 }
 
 void main()
 {
-    int s;//???????
-    int w[15];//?????
-    int p[15];//?????
-    int x[15];//???????
+    int s;//获得的最大价值
+    int w[15];//物品的重量
+    int p[15];//物品的价值
+    int x[15];//物品的选取状态
     int n,i;
-    int M;//??????
+    int M;//背包最大容量
 
     scanf("%d %d",&M, &n);
 
